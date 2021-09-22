89a9ff4358894a60f82f8774b52e655bc22e7c5f
OlhaNester <olga_nv@ukr.net>
2021-09-22 19:50:01 +0300

    444

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
+import FeedbackOptions from "./Component/FeedbackOptions/FeedbackOptions";
 
 class App extends Component {
   state = {
@@ -14,60 +14,53 @@ class App extends Component {
   //   }));
   // };
   handleGood = () => {
-   
-     this.setState(prevState => {
+    this.setState((prevState) => {
       return {
         good: prevState.good + 1,
-              };
+      };
     });
   };
 
   handleNeutral = () => {
-        this.setState(prevState => {
+    this.setState((prevState) => {
       return {
-        neutral: prevState.neutral + 1,        
+        neutral: prevState.neutral + 1,
       };
     });
   };
 
   handleBad = () => {
-    this.setState(prevState => {
+    this.setState((prevState) => {
       return {
         bad: prevState.bad + 1,
       };
     });
   };
-  
-  
-  render() {
-    const countTotalFeedback =
-    Object.values(this.state).reduce(function (a, b) {
-      return a + b
-    });
-    
-    const percentageGood = Math.round((this.state.good / countTotalFeedback) * 100);
 
-  return (
-    <div className="App">
-      <h1>Please leave feedback </h1>
-      <div>
-        <button type="button" onClick={this.handleGood} >Good</button>
-        <button type="button" onClick={ this.handleNeutral}>Neutral</button>
-      <button type="button" onClick={this.handleBad}>Bad</button>
-      </div>
-      <div>
-        <h1>Statistics</h1>
+  render() {
+    const total = Object.values(this.state).reduce(function (a, b) {
+      return a + b;
+    });
+
+    const positivePercentage = Math.round((this.state.good / total) * 100);
+
+    return (
+      <div className="App">
+        <h1>Please leave feedback </h1>
+        <FeedbackOptions />
         <div>
-          <p>Good: { this.state.good}</p>
-          <p>Neutral:{ this.state.neutral}</p>
-          <p>Bad:{ this.state.bad}</p>
-          <p>Total:{ countTotalFeedback}</p>
-          <p>Positive Feedback: { percentageGood}</p>
+          <h1>Statistics</h1>
+          <div>
+            <p>Good: {this.state.good}</p>
+            <p>Neutral:{this.state.neutral}</p>
+            <p>Bad:{this.state.bad}</p>
+            <p>Total:{total}</p>
+            <p>Positive Feedback: {positivePercentage}%</p>
+          </div>
         </div>
       </div>
-      
-    </div>
-  );
-}}
+    );
+  }
+}
 
 export default App;
---
+++ src/Component/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const FeedbackOptions = () => {
+  <div>
+    <button type="button" onClick={this.handleGood}>
+      Good
+    </button>
+    <button type="button" onClick={this.handleNeutral}>
+      Neutral
+    </button>
+    <button type="button" onClick={this.handleBad}>
+      Bad
+    </button>
+  </div>;
+};
+
+export default FeedbackOptions;
